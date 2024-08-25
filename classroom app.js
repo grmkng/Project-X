ad62465ab236481e7d30349122c6f47810ce89f9
grmkng <168589585+grmkng@users.noreply.github.com>
2024-08-25 21:33:13 +0100

    classroom app

---
+++ classroom app.js
@@ -0,0 +1,20 @@
+/**
+ * @param {array} grades
+ */
+export function getNumberOfGrades(grades) {
+    return grades.length
+}
+
+/**
+ * @param {array} grades
+ */
+export function getFirstGrade(grades) {
+    return grades[0]
+}
+
+/**
+ * @param {array} grades
+ */
+export function getLastGrade(grades) {
+    return grades[grades.length - 1]
+}
